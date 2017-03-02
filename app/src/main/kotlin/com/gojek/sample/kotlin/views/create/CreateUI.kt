88edc535820e441827674daf206f069448b52d3a
Budi Oktaviyan Suryanto <budi.suryanto@go-jek.com>
2017-03-02 17:29:11 +0700

    Add screen create

--- app/src/main/kotlin/com/gojek/sample/kotlin/views/create/CreateUI.kt
+++ app/src/main/kotlin/com/gojek/sample/kotlin/views/create/CreateUI.kt
@@ -2,6 +2,7 @@ package com.gojek.sample.kotlin.views.create
 
 import android.annotation.SuppressLint
 import android.support.v4.content.ContextCompat
+import android.text.InputType
 import android.widget.ImageView
 import com.gojek.sample.kotlin.R
 import org.jetbrains.anko.*
@@ -36,15 +37,114 @@ class CreateUI : AnkoComponent<CreateActivity> {
             scrollView {
                 id = R.id.sv_create
                 isFillViewport = true
+                isScrollContainer = true
 
                 verticalLayout {
+                    isFocusable = true
+                    isFocusableInTouchMode = true
+
                     imageView {
                         id = R.id.iv_create_photo
                         adjustViewBounds = true
                         scaleType = ImageView.ScaleType.CENTER_CROP
+                        background = ContextCompat.getDrawable(ctx, R.drawable.bg_camera)
                     }.lparams {
                         width = matchParent
                         height = dip(200)
+                    }
+
+                    textView {
+                        textColor = ContextCompat.getColor(ctx, R.color.colorPrimaryDark)
+                        textSize = 16f
+                        text = ctx.resources.getString(R.string.text_firstname)
+                    }.lparams {
+                        width = matchParent
+                        height = wrapContent
+                        margin = dip(8)
+                    }
+
+                    editText {
+                        id = R.id.et_create_firstname
+                        textColor = ContextCompat.getColor(ctx, R.color.colorAccent)
+                        textSize = 14f
+                        hint = ctx.resources.getString(R.string.hint_firstname)
+                    }.lparams {
+                        width = matchParent
+                        height = wrapContent
+                        topMargin = dip(4)
+                        leftMargin = dip(8)
+                        rightMargin = dip(8)
+                    }
+
+                    textView {
+                        textColor = ContextCompat.getColor(ctx, R.color.colorPrimaryDark)
+                        textSize = 16f
+                        text = ctx.resources.getString(R.string.text_lastname)
+                    }.lparams {
+                        width = matchParent
+                        height = wrapContent
+                        margin = dip(8)
+                    }
+
+                    editText {
+                        id = R.id.et_create_lastname
+                        textColor = ContextCompat.getColor(ctx, R.color.colorAccent)
+                        textSize = 14f
+                        hint = ctx.resources.getString(R.string.hint_lastname)
+                    }.lparams {
+                        width = matchParent
+                        height = wrapContent
+                        topMargin = dip(4)
+                        leftMargin = dip(8)
+                        rightMargin = dip(8)
+                    }
+
+                    textView {
+                        textColor = ContextCompat.getColor(ctx, R.color.colorPrimaryDark)
+                        textSize = 16f
+                        text = ctx.resources.getString(R.string.text_email)
+                    }.lparams {
+                        width = matchParent
+                        height = wrapContent
+                        margin = dip(8)
+                    }
+
+                    editText {
+                        id = R.id.et_create_email
+                        textColor = ContextCompat.getColor(ctx, R.color.colorAccent)
+                        textSize = 14f
+                        hint = ctx.resources.getString(R.string.hint_email)
+                        inputType = InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+                    }.lparams {
+                        width = matchParent
+                        height = wrapContent
+                        topMargin = dip(4)
+                        leftMargin = dip(8)
+                        rightMargin = dip(8)
+                    }
+
+                    textView {
+                        textColor = ContextCompat.getColor(ctx, R.color.colorPrimaryDark)
+                        textSize = 16f
+                        text = ctx.resources.getString(R.string.text_phonenumber)
+                    }.lparams {
+                        width = matchParent
+                        height = wrapContent
+                        margin = dip(8)
+                    }
+
+                    editText {
+                        id = R.id.et_create_phonenumber
+                        textColor = ContextCompat.getColor(ctx, R.color.colorAccent)
+                        textSize = 14f
+                        hint = ctx.resources.getString(R.string.hint_email)
+                        inputType = InputType.TYPE_TEXT_VARIATION_PHONETIC
+                    }.lparams {
+                        width = matchParent
+                        height = wrapContent
+                        topMargin = dip(4)
+                        leftMargin = dip(8)
+                        rightMargin = dip(8)
                     }
                 }
             }.lparams {
